1b9705eb7ded36262f39069f51df2ee7f16fe59b
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-12-05 01:15:52 -0600

    Deploy

--- src/components/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState, useEffect, useRef} from 'react';
-
-export default function Homepage (){
-    return (
-        <div>
-            <span>Welcome to my portfolio</span>
-        </div>
-    );
-};
