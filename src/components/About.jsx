cebc641636e78bdadbb37db3c02e4a4e5dc2957d
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-12-05 00:01:26 -0600

    About + Tab Icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,16 @@ import { Image } from 'antd';
 
 const About = () => {
     return(
-        <div>
-            <h1>Learn about me here!</h1>
+        <div style={{ margin: '1rem' }}>
+            <h2>Learn about me here!</h2>
 
             <Image
                 width={200}
                 src="/picofme.jpg"
             />
+
+            <h1>Who I am:</h1>
+            <p style={{ margin: '2rem' }}>I am a junior full-stack developer, but it's been a journey to get here. Two years ago I got my B.S. in Geology at UT El Paso. I've been a project manager and an assistant production manager at a plant. And for nearly a decade I was in the service industry. And now I take all the skills and lessons from those experiences, the support of the folks I met along the way, add a smidge of hacking away at a keyboard, and I get to make cutting edge web apps-- how cool is that?</p>
         </div>
     );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 
-import { YahooOutlined, GithubOutlined } from '@ant-design/icons';
+import { MediumOutlined, GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
 
@@ -9,15 +9,19 @@ export default function Myfooter (){
     return (
     <Footer style={{ textAlign: 'center', position: 'absolute', bottom: 0, width: '100%' }}>
 
-        <Menu mode="horizontal" defaultSelectedKeys={['about']}>
+        <Menu mode="horizontal">
             <span>Matt Lawhorn ©2022</span>
 
             <Menu.Item key="github" icon={<GithubOutlined />}>
-                <a class="nav-link" href="/about">My GitHub</a>
+                <a class="nav-link" href="https://github.com/Lawhornmatt">My GitHub</a>
             </Menu.Item>
 
-            <Menu.Item key="email" icon={<YahooOutlined />}>
-                <a class="nav-link" href="/about">Email Me</a>
+            <Menu.Item key="linkedin" icon={<LinkedinOutlined />}>
+                <a class="nav-link" href="https://www.linkedin.com/in/matthew-lawhorn/">My LinkedIn</a>
+            </Menu.Item>
+
+            <Menu.Item key="medium" icon={<MediumOutlined />}>
+                <a class="nav-link" href="https://www.zombo.com/">My Medium</a>
             </Menu.Item>
         </Menu>
     </Footer>
